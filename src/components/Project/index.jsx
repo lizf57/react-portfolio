2560edd8a1ee5c4f68d3fbb00836f174e59e7592
mona lisa <lizfriedman5@gmail.com>
2023-10-31 18:51:26 -0500

    changed to single project component used multiple times

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,5 +1,57 @@
 import './project.css'
 
+const projects = [
+   {
+      title: "Career Craft",
+      image: "/images/career-craft.png",
+      technologies: "Node | Express | Sequelize",
+      description: "Career Craft is a web application that generates resumes to a PDF for users to download",
+      deployedUrl: "https://career-craft3-1f46cd29a2af.herokuapp.com/login",
+      repoUrl: "https://github.com/tjansson-ui/JobReadyResumeBuilder",
+   },
+   {
+      title: "Work Day Schedule",
+      image: "/images/office-organizer-img.jpeg",
+      technologies: "jQuery | HTML | CSS",
+      description: "A straightforward calender application that facilitates users in preserving events for every hour wihtin a conventional working day from 9am to 5pm.",
+      deployedUrl: "https://lizf57.github.io/work-day-schedule/",
+      repoUrl: "https://github.com/lizf57/work-day-schedule"
+   },
+   {
+      title: "Weather App",
+      image: "/images/weather-app-img.jpeg",
+      technologies: "Third Party API | Javascript",
+      description: "The Weather App enables users to access weather information for any city, including a comprehensive 5-day forecast.",
+      deployedUrl: "https://lizf57.github.io/weather-app/",
+      repoUrl: "https://github.com/lizf57/weather-app"
+   },
+   {
+      title: "Culinary Climate",
+      image: "/public/images/cc-logo.png",
+      technologies: "Two Server Side API's | CSS",
+      description: "The Culinary Climate application has been crafted to assist users in making dinner menu decisions by leveraging weather forecast data.",
+      deployedUrl: "https://lshankkirchner017.github.io/culinary-climate-uofw/",
+      repoUrl: "https://github.com/LShankKirchner017/culinary-climate-uofw"
+   },
+   {
+      title: "Password Generator",
+      image: "/public/images/password.jpeg",
+      technologies: "HTML | CSS | Javascript",
+      description: "The Password Generator app empowers users to create randomized passwords using a combination of lowercase, uppercase, numeric, and special characters.",
+      deployedUrl: "https://lizf57.github.io/password_generator/",
+      repoUrl: "https://github.com/lizf57/password_generator"
+   },
+   {
+      title: "SVG Logo Maker",
+      image: "/public/images/logo-maker.jpeg",
+      technologies: "Node | Inquirer | Jest",
+      description: "SVG Logo Maker functions through Node JS command line using Inquirer for collecting input from the user to generate a logo to be saved as an SVG file.",
+      deployedUrl: "https://github.com/lizf57/svg-logo-maker",
+      repoUrl: "https://github.com/lizf57/svg-logo-maker"
+   },
+
+]
+
 const Project = () => {
     return (
      <>
@@ -7,97 +59,26 @@ const Project = () => {
          <div className='container m-1'>
             <div className='row justify-content-center d-flex m-1'>
 
-         {/* project 1 */}
-            <div className='col-lg-3 gap-1 m-1'>
-               <div className='card'>
-                  <h1 className='text-center rounded-2 fs-2 p-3 headline'>Career Craft</h1>
-                  <img className='card-img-top' src="/images/career-craft.png" alt="Card image"/>
-                  <div className='card-body'>
-                     <h4 className='card-title text-center fs-4'> Node | Express | Sequelize</h4>
-                     <p className='card-text text-center fs-5'>Career Craft is a web application that generates resumes to a PDF for users to download</p>
-                     <div className='btns d-flex justify-content-center gap-3'>
-                        <a href='https://career-craft3-1f46cd29a2af.herokuapp.com/login' className='btn' target='_blank'>Deploy App</a>
-                        <a href='https://github.com/tjansson-ui/JobReadyResumeBuilder' className='btn' target='_blank'>Github Repo</a>
+         {projects.map(project => {
+            return (
+               <div className='col-lg-3 gap-1 m-1'>
+                  <div className='card'>
+                     <h1 className='text-center rounded-2 fs-2 p-3 headline'>{project.title}</h1>
+                     <img className='card-img-top' src={project.image} alt="Card image"/>
+                     <div className='card-body'>
+                        <h4 className='card-title text-center fs-4'> {project.technologies}</h4>
+                        <p className='card-text text-center fs-5'>{project.description}</p>
+                        <div className='btns d-flex justify-content-center gap-3'>
+                           <a href={project.deployedUrl} className='btn' target='_blank'>Deploy App</a>
+                           <a href={project.repoUrl} className='btn' target='_blank'>Github Repo</a>
+                        </div>
                      </div>
                   </div>
                </div>
-            </div>
-         {/* project 2 */}
-         <div className='col-lg-3 m-1 gap-1'>
-               <div className='card'>
-                  <h1 className='text-center rounded-2 fs-2 p-3 headline'>Work Day Schedule</h1>
-                  <img className='card-img-top' src="/images/office-organizer-img.jpeg" alt="Card image"/>
-                  <div className='card-body'>
-                     <h4 className='card-title text-center fs-4'> jQuery | HTML | CSS</h4>
-                     <p className='card-text text-center fs-5'>A straightforward calender application that facilitates users in preserving events for every hour wihtin a conventional working day from 9am to 5pm.</p>
-                     <div className='btns d-flex justify-content-center gap-3'>
-                        <a href='https://lizf57.github.io/work-day-schedule/' className='btn' target='_blank'>Deploy App</a>
-                        <a href='https://github.com/lizf57/work-day-schedule' className='btn' target='_blank'>Github Repo</a>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         {/* project 3 */}
-         <div className='col-lg-3 m-1 gap-1'>
-               <div className='card'>
-                  <h1 className='text-center rounded-2 fs-2 p-3 headline'>Weather App</h1>
-                  <img className='card-img-top' src="/images/weather-app-img.jpeg" alt="Card image"/>
-                  <div className='card-body'>
-                     <h4 className='card-title text-center fs-4'> Third Party API | Javascript </h4>
-                     <p className='card-text text-center fs-5'>The Weather App enables users to access weather information for any city, including a comprehensive 5-day forecast.</p>
-                     <div className='btns d-flex justify-content-center gap-3'>
-                        <a href='https://lizf57.github.io/weather-app/' className='btn' target='_blank'>Deploy App</a>
-                        <a href='https://github.com/lizf57/weather-app' className='btn' target='_blank'>Github Repo</a>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         {/* project 4 */}
-         <div className='col-lg-3 m-1 gap-1'>
-               <div className='card'>
-                  <h1 className='text-center rounded-2 fs-2 p-3 headline'>Culinary Climate</h1>
-                  <img className='card-img-top' src="/public/images/cc-logo.png" alt="Card image"/>
-                  <div className='card-body'>
-                     <h4 className='card-title text-center fs-4'> Third Party API | Javascript </h4>
-                     <p className='card-text text-center fs-5'>The Weather App enables users to access weather information for any city, including a comprehensive 5-day forecast.</p>
-                     <div className='btns d-flex justify-content-center gap-3'>
-                        <a href='https://lshankkirchner017.github.io/culinary-climate-uofw/' className='btn' target='_blank'>Deploy App</a>
-                        <a href='https://github.com/LShankKirchner017/culinary-climate-uofw' className='btn' target='_blank'>Github Repo</a>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         {/* project 5 */}
-         <div className='col-lg-3 m-1 gap-1'>
-               <div className='card'>
-                  <h1 className='text-center rounded-2 fs-2 p-3 headline'>Password Generator</h1>
-                  <img className='card-img-top' src="/public/images/password.jpeg" alt="Card image"/>
-                  <div className='card-body'>
-                     <h4 className='card-title text-center fs-4'> HTML | CSS | Javascript </h4>
-                     <p className='card-text text-center fs-5'>The Password Generator app empowers users to create randomized passwords using a combination of lowercase, uppercase, numeric, and special characters.</p>
-                     <div className='btns d-flex justify-content-center gap-3'>
-                        <a href='https://lizf57.github.io/password_generator/' className='btn' target='_blank'>Deploy App</a>
-                        <a href='https://github.com/lizf57/password_generator' className='btn' target='_blank'>Github Repo</a>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         {/* project 6 */}
-         <div className='col-lg-3 m-1 gap-1'>
-               <div className='card'>
-                  <h1 className='text-center rounded-2 fs-2 p-3 headline'>SVG Logo Maker</h1>
-                  <img className='card-img-top' src="/public/images/logo-maker.jpeg" alt="Card image"/>
-                  <div className='card-body'>
-                     <h4 className='card-title text-center fs-4'> Node | Inquirer | Jest </h4>
-                     <p className='card-text text-center fs-5'>SVG Logo Maker functions through Node JS command line using Inquirer for collecting input from the user to generate a logo to be saved as an SVG file.</p>
-                     <div className='btns d-flex justify-content-center gap-3'>
-                        <a href='https://github.com/lizf57/svg-logo-maker' className='btn' target='_blank'>Github Repo</a>
-                     </div>
-                  </div>
-               </div>
-            </div>
 
-            {/* end of projects */}
+            )
+         })}
+
             </div>
          </div>
       </div>
